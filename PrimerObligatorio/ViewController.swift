f35ceee538cd55661c04229c096f652977af4da3
Amparo Iglesias <ampiglesiasgo@gmail.com>
2019-04-23 22:35:56 -0300

    Initial Commit

---
+++ PrimerObligatorio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerObligatorio
+//
+//  Created by Amparo Iglesias on 4/23/19.
+//  Copyright © 2019 Amparo Iglesias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
